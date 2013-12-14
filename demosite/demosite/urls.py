da0c6c67fcb7ec171815591494200d9d4a16c757
kevin <null386@gmail.com>
2013-12-14 13:06:39 +0800

    完成动态显示时间点url配置

--- demosite/demosite/urls.py
+++ demosite/demosite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from demosite.views import hello
+from demosite.views import hello,create_time
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^hello/$', hello),
+	url(r'^time/$', create_time),
 )
--- demosite/demosite/views.py
+++ demosite/demosite/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 import datetime
 
 def hello(request):
-	#return HttpResponse("Hello world")
+	return HttpResponse("Hello world")
+
+def create_time(request):
 	now = datetime.datetime.now()
 	html = "<html><body>It is now %s.</body></html>" % now
 	return HttpResponse(html)
