e08b5f7f04661cfbfbc326f0ae1ee6f6422b77ea
kevin <null386@gmail.com>
2013-12-14 12:58:20 +0800

    显示动态点django页面内容

--- demosite/demosite/views.py
+++ demosite/demosite/views.py
@@ -1,5 +1,9 @@
 #-*- coding:utf-8 -*-
 from django.http import HttpResponse
+import datetime
 
 def hello(request):
-	return HttpResponse("Hello world")
+	#return HttpResponse("Hello world")
+	now = datetime.datetime.now()
+	html = "<html><body>It is now %s.</body></html>" % now
+	return HttpResponse(html)
